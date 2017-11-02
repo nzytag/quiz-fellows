f4bf2d4432a692fa434a2cb9078a3b879b6af3ce
Hector <norzagaray@me.com>
2017-11-02 14:53:58 -0700

    added greeting to index and flash pages

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+//this will change the: welcome to quiz fellows depending on the time of the day
+//this may move to a different js file
+
+var today =  new Date ();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+  greeting = 'Good Evening! Welcome to Quiz Fellows!';
+} else if (hourNow > 12) {
+  greeting = 'Good Afternoon! Welcome to Quiz Fellows!';
+} else if (hourNow > 0) {
+  greeting = 'Good Morning! Welcome to Quiz Fellows';
+} else {
+  greeting = 'Welcome!';
+}
+
+document.write('<h1>' + greeting + '<h/1>');

217574f58e7b74bfa27e47ee861c8439e164eb68
Melanie Downing <sayanything830@gmail.com>
2017-11-04 13:59:43 -0700

    trying to get page reloading properly

--- jsprobs.js
+++ jsprobs.js
@@ -5,17 +5,17 @@ var jsProblems = [
   'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'
 ]; //array of JS problems
 var allJsObj = []; //array of JS problem objects
-var prevJs = [];
-var currJs = [];
+var prevJs = [0,1,2,3,4,5];
+var currJs = [6,7,8,9,10,11];
 var probsOnDom = [
   document.getElementById('top-left'),
   document.getElementById('top-center'),
   document.getElementById('top-right'),
   document.getElementById('bottom-left'),
-  //document.getElementById('bottom-center'),
-  //document.getElementById('bottom-right')
+  document.getElementById('bottom-center'),
+  document.getElementById('bottom-right')
 ];
-var btn = document.getElementById('refresh');
+var btn = document.getElementsByTagName('button');
 
 function ProblemObject(problem) {
   this.problem = problem;
@@ -32,16 +32,40 @@ for (var a = 0; a < jsProblems.length; a ++) {
   allJsObj.push(new ProblemObject(jsProblems[a]));
 }
 
+compareIndex();
 
 //*** Creates random number for range of index items of JS objects
-function randNumJsIndex () {
-  var randNum = Math.floor(Math.random() * allJsObj.length);
-  for (var b = 0, b < prevJs.length; b ++) {
-    while (randNum === prevJs[b]) {
-      randNum = Math.floor(Math.random() * allJsObj.length);
+function randIndexGen () {
+  var randIndex = Math.floor(Math.random() * allJsObj.length);
+  for (var b = 0; b < prevJs.length; b ++) {
+    while (randIndex === prevJs[0] || randIndex === prevJs[1] || randIndex === prevJs[2] || randIndex === prevJs[3] || randIndex === prevJs[4] || randIndex === prevJs[5]) {
+      randIndex = Math.floor(Math.random() * allJsObj.length);
     }
   }
-  return randNum;
+  return randIndex;
+}
+
+//*** helper function that compares index numbers
+function compareIndex () {
+  for (var c = 0; c < currJs.length; c ++) { //for each index in the array
+    currJs[c] = randIndexGen();
+    while (currJs[1] === currJs[0]) { //check the index to the others...
+      currJs[1] = randIndexGen(); //otherwise generate new random index number
+    }
+    while (currJs[2] === currJs[1] || currJs[2] === currJs[0]) {
+      currJs[2] = randIndexGen();
+    }
+    while (currJs[3] === currJs[2] || currJs[3] === currJs[1] || currJs[3] === currJs[0]) {
+      currJs[3] = randIndexGen();
+    }
+    while (currJs[4] === currJs[3] || currJs[4] === currJs[2] || currJs[4] === currJs[1] || currJs[4] === currJs[0]) {
+      currJs[4] = randIndexGen();
+    }
+    while (currJs[5] === currJs[4] || currJs[5] === currJs[3] || currJs[5] === currJs[2] || currJs[5] === currJs[1] || currJs[5] === currJs[0]) {
+      currJs[5] = randIndexGen();
+    }
+    prevJs = currJs; //makes current images into previous images and not available for next page refresh
+  }
 }
 
 //*** Helper function to recognize index of JS problem objects
@@ -53,19 +77,9 @@ var probIndex = function (index) {
 var updateProbsSeen = function () {
   for (var i = 0; i < probsOnDom.length; i ++) {
     probIndex(prevJs[i]).seen = false;
-    prevJs[i] = currJs[i];
-    while (probIndex(currJs[i]).seen === true) {
-      currJs[i] = randNumJsIndex();
-    }
-    probIndex(currJs[i]).seen = true;
+    //probIndex(currJs[i]).seen = true;
   }
 };
-
-function seenToTrue () {
-  for (var j = 0; j < probsOnDom.length; j ++) {
-    probIndex(currJs[j]).seen = true;
-  }
-}
 
 function newProbs () {
   for (var k = 0; k < probsOnDom.length; k ++) {
@@ -76,8 +90,7 @@ function newProbs () {
 
 function refreshBtn (event) {
   updateProbsSeen();
-  seenToTrue();
   newProbs();
 };
 
-btn.addEventListener('click', refreshBtn);
+btn.addEventListener('submit', refreshBtn);

9fb6f2e11e65dd37cab5908ae1ad8316cd2e152b
Melanie Downing <sayanything830@gmail.com>
2017-11-06 11:47:29 -0800

    created scaffold for each topic of flashcards

--- script/cssflashcard.js
+++ script/cssflashcard.js
@@ -3,7 +3,7 @@
 
 var problem = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg'];
 
-var answer = ['pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg']
+var answer = ['pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
 var topLeft = document.getElementById('top-left');
 var topCenter = document.getElementById('top-center');

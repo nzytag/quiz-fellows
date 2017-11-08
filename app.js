ed20318862d1030fc37ab6d51ac5b471872c9406
Mitchell M <mitchellm1618@gmail.com>
2017-11-08 13:47:09 -0800

    removed unnecessary files, and adding check form functionality

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ This is the file that links to index.html, it will display a greeting that chang
 
 'use strict';
 
-//*** changes the: welcome to quiz fellows depending on the time of the day ***
-function displayTime() {
-  var today =  new Date ();
-  var hourNow = today.getHours();
-  var greeting;
-
-  if (hourNow > 18) {
-    greeting = 'Good Evening! Welcome to Quiz Fellows!';
-  } else if (hourNow > 12) {
-    greeting = 'Good Afternoon! Welcome to Quiz Fellows!';
-  } else if (hourNow > 0) {
-    greeting = 'Good Morning! Welcome to Quiz Fellows';
-  } else {
-    greeting = 'Welcome!';
-  }
-
-  document.write('<h1>' + greeting + '<h/1>');
-}
-displayTime(); //calls the date function
+// //*** changes the: welcome to quiz fellows depending on the time of the day ***
+// function displayTime() {
+//   var today =  new Date ();
+//   var hourNow = today.getHours();
+//   var greeting;
+//
+//   if (hourNow > 18) {
+//     greeting = 'Good Evening! Welcome to Quiz Fellows!';
+//   } else if (hourNow > 12) {
+//     greeting = 'Good Afternoon! Welcome to Quiz Fellows!';
+//   } else if (hourNow > 0) {
+//     greeting = 'Good Morning! Welcome to Quiz Fellows';
+//   } else {
+//     greeting = 'Welcome!';
+//   }
+//
+//   document.write('<h1>' + greeting + '<h/1>');
+// }
+// displayTime(); //calls the date function
 
 //*** function to take user to corepsonding topic site that they have selected ***
 var form = document.getElementById('form');
